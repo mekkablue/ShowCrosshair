62cdcea7d6f85a3c78a57947304ecc1289b68240
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2018-01-01 15:28:13 +0100

    Added French and Spanish localisation, improved context menu

--- ShowCrosshair.glyphsReporter/Contents/Resources/plugin.py
+++ ShowCrosshair.glyphsReporter/Contents/Resources/plugin.py
@@ -18,7 +18,12 @@ import math
 class ShowCrosshair(ReporterPlugin):
 
 	def settings(self):
-		self.menuName = Glyphs.localize({'en': u'Crosshair', 'de': u'Fadenkreuz'})
+		self.menuName = Glyphs.localize({
+			'en': u'Crosshair', 
+			'de': u'Fadenkreuz',
+			'es': u'cruz',
+			'fr': u'réticule',
+		})
 		NSUserDefaults.standardUserDefaults().registerDefaults_({
 				"com.mekkablue.ShowCrosshair.universalCrosshair": 1,
 				"com.mekkablue.ShowCrosshair.showCoordinates": 0,
@@ -26,22 +31,6 @@ class ShowCrosshair(ReporterPlugin):
 			})
 		# self.universalCrosshair = bool( Glyphs.defaults["com.mekkablue.ShowCrosshair.universalCrosshair"] )
 		
-		self.generalContextMenus = [
-			{
-				'name': Glyphs.localize({
-					'en': u'Toggle Crosshair',
-					'de': u'Fadenkreuz umschalten'
-				}), 
-				'action': self.toggleUniversalCrosshair
-			},
-			{
-				'name': Glyphs.localize({
-					'en': u'Toggle Coordinates',
-					'de': u'Koordinaten ein/ausblenden'
-				}), 
-				'action': self.toggleCoordinates
-			}
-		]
 		
 	def background(self, layer):
 		toolEventHandler = self.controller.view().window().windowController().toolEventHandler()
@@ -83,7 +72,7 @@ class ShowCrosshair(ReporterPlugin):
 		
 		if bool(Glyphs.defaults["com.mekkablue.ShowCrosshair.showCoordinates"]) and not toolIsTextTool:
 			mousePosition = self.mousePosition()
-			coordinateText = "% 4i, % 4i" % (
+			coordinateText = "%4d, %4d" % (
 				round(mousePosition.x), 
 				round(mousePosition.y)
 			)
@@ -120,8 +109,66 @@ class ShowCrosshair(ReporterPlugin):
 			NSLog(traceback.format_exc())
 	
 	def toggleUniversalCrosshair(self):
-		Glyphs.defaults["com.mekkablue.ShowCrosshair.universalCrosshair"] = int(not bool(Glyphs.defaults["com.mekkablue.ShowCrosshair.universalCrosshair"]))
+		self.toggleSetting("universalCrosshair")
 		
-	def toggleCoordinates(self):
-		Glyphs.defaults["com.mekkablue.ShowCrosshair.showCoordinates"] = int(not bool(Glyphs.defaults["com.mekkablue.ShowCrosshair.showCoordinates"]))
+	def toggleShowCoordinates(self):
+		self.toggleSetting("showCoordinates")
+	
+	def conditionalContextMenus(self):
+		return [
+		{
+			'name': Glyphs.localize({
+				'en': u"Crosshair Options:", 
+				'de': u"Fadenkreuz-Einstellungen:", 
+				'es': u"Opciones de la cruz:", 
+				'fr': u"Options pour le réticule:",
+				}), 
+			'action': None,
+		},
+		{
+			'name': Glyphs.localize({
+				'en': u"Always Show Crosshair", 
+				'de': u"Fadenkreuz immer anzeigen", 
+				'es': u"Siempre mostrar la cruz", 
+				'fr': u"Toujours montrer le réticule",
+				}), 
+			'action': self.toggleUniversalCrosshair,
+			'state': Glyphs.defaults[ "com.mekkablue.ShowCrosshair.universalCrosshair" ],
+		},
+		{
+			'name': Glyphs.localize({
+				'en': u"Show Coordinates", 
+				'de': u"Koordinaten anzeigen", 
+				'es': u"Mostrar coordinados", 
+				'fr': u"Montrer coordonnées",
+				}), 
+			'action': self.toggleShowCoordinates,
+			'state': Glyphs.defaults[ "com.mekkablue.ShowCrosshair.showCoordinates" ],
+		},
+		]
 		
+	
+	def toggleSetting(self, prefName):
+		pref = "com.mekkablue.ShowCrosshair.%s" % prefName
+		oldSetting = bool(Glyphs.defaults[pref])
+		Glyphs.defaults[pref] = int(not oldSetting)
+	
+	def addMenuItemsForEvent_toMenu_(self, event, contextMenu):
+		'''
+		The event can tell you where the user had clicked.
+		'''
+		try:
+			
+			if self.generalContextMenus:
+				setUpMenuHelper(contextMenu, self.generalContextMenus, self)
+			
+			newSeparator = NSMenuItem.separatorItem()
+			contextMenu.addItem_(newSeparator)
+			
+			contextMenus = self.conditionalContextMenus()
+			if contextMenus:
+				setUpMenuHelper(contextMenu, contextMenus, self)
+		
+		except:
+			self.logError(traceback.format_exc())
+	
